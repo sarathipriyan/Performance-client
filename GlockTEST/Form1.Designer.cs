3f0262a9aa265193d613e363cc82a6ae50803e19
smani <smani@terafastnet.com>
2019-09-03 00:28:39 -0700

    first commit

---
+++ GlockTEST/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace GlockTEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Token";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(854, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(475, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USER_NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(475, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PASS_WORD";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(604, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(604, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(60, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "UID_NAME";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(225, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(119, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(58, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "NO_REQUEST";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(225, 129);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(119, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1067, 675);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(59, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "KYC_REASON";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(60, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 18);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "KYC_RESULT";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Sitka Small", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(495, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(324, 30);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Goldilock Performance Testing";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SUCCESS",
+            "FAILED",
+            "ANYVALUE",
+            "REVIEW"});
+            this.comboBox1.Location = new System.Drawing.Point(223, 316);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "SUCCESS",
+            "FAILED",
+            "ANYVALUE",
+            "REVIEW"});
+            this.comboBox2.Location = new System.Drawing.Point(223, 364);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(225, 444);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(852, 215);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(809, 198);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "TotalTime";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(896, 195);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(181, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GlockTEST.Properties.Resources.meta_3eaa7fde;
+            this.ClientSize = new System.Drawing.Size(1252, 733);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ GlockTEST/Form1.cs
@@ -0,0 +1,208 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlockTEST
+{
+    public partial class Form1 : Form
+    {
+        public static List<modelproperty> listvalue = new List<modelproperty>();
+        KYC_VALUE value = new KYC_VALUE();
+        modelproperty value1 = new modelproperty();
+        static readonly object _object = new object();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static List<Task> TaskList = new List<Task>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GETTOKEN();
+        }
+        public void GETTOKEN()
+        {
+            try
+            {
+                string webAddr = "http://10.6.5.15:8080/auth/token";
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+                httpWebRequest.ContentType = "application/json; charset=utf-8";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{ \"password\":" + "\"" + textBox3.Text + "\", \"username\":" + "\"" + textBox2.Text + "\"}";
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                }
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var responseText = streamReader.ReadToEnd();
+                    Console.WriteLine(responseText);
+                    dynamic data = JObject.Parse(responseText);
+                    //Now you have your response.
+                    //or false depending on information in the response    
+                    string value = "Bearer";
+
+                    string title = data.bearerToken;
+
+                    textBox1.Text = value + " " + " " + title;
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch1 = new Stopwatch();
+            //ist value2 = new List();
+            int norquest = Convert.ToInt16(textBox5.Text);
+            stopWatch1.Start();
+            POSTUID(norquest);
+            stopWatch1.Stop();
+            TimeSpan ts = stopWatch1.Elapsed;
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            textBox6.Text = elapsedTime;
+
+
+        }
+
+        private void POSTUID(int norquest)
+        {
+
+
+            for (int i = 1; i <= norquest; i++)
+            {
+                string NAME = textBox4.Text + i;
+                value.Kycreason = comboBox1.Text;
+                value.Kycresult = comboBox2.Text;
+                Task fooWrappedInTask = Task.Run(() => RunMe(NAME));
+                TaskList.Add(fooWrappedInTask);
+            }
+            Task.WaitAll(TaskList.ToArray());
+            // Get the elapsed time as a TimeSpan value.
+
+            //listvalue.Add(value1);
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("NAME");
+            dataTable.Columns.Add("/ident/{identId}");
+            dataTable.Columns.Add("/ident/{identId}/result");
+            dataTable.Columns.Add("TaskStart_Time");
+            dataTable.Columns.Add("TaskEnd_Time");
+            //dataTable.Columns.Add("Total_Time");
+            DataRow row = null;
+            //listvalue.Add(value1);
+            foreach (var rowObj in listvalue)
+            {
+                row = dataTable.NewRow();
+                dataTable.Rows.Add(rowObj.name, rowObj.API1_Status, rowObj.API2_Status, rowObj.TaskStart, rowObj.TaskEnd);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+        public Task<string> RunMe(string NAME)
+        {
+            modelproperty value1 = new modelproperty();
+            value1.name = NAME;
+            Stopwatch stopWatch = new Stopwatch();
+            value1.TaskStart = DateTime.Now.ToString("HH:mm:ss");
+            stopWatch.Start();
+            string webAddr = "http://10.6.5.15:8080/ident/" + NAME;
+            string status = string.Empty;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+            try
+            {
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{ \"birthday\": \"string\", \"birthplace\": \"string\", \"city\": \"string\", \"country\": \"string\", \"email\": \"string\", \"firstname\": \"string\", \"gender\": \"string\", \"lastname\": \"string\", \"mobilephone\": \"string\", \"nationality\": \"string\", \"street\": \"string\", \"zipcode\": \"string\"}";
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                }
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                status = httpResponse.StatusCode.ToString();
+                value1.API1_Status = status;
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var responseText = streamReader.ReadToEnd();
+                    Console.WriteLine(responseText);
+                }
+                httpResponse.Close();
+                if (status == "OK")
+                {
+
+                    string webAddr1 = "http://10.6.5.15:8080/ident/" + NAME + "/result";
+                    string status1 = string.Empty;
+                    var httpWebRequest1 = (HttpWebRequest)WebRequest.Create(webAddr1);
+                    httpWebRequest1.ContentType = "application/json; charset=utf-8";
+                    httpWebRequest1.Method = "POST";
+                    httpWebRequest1.Headers.Add("Authorization", "dcmGV25hZFzc3VudDM6cGzdCdvQ=");
+
+
+                    using (var streamWriter1 = new StreamWriter(httpWebRequest1.GetRequestStream()))
+                    {
+                        string json = "{ \"kycReason\":" + "\"" + value.Kycreason + "\", \"kycResult\":" + "\"" + value.Kycresult + "\", \"personSearchInfo\": { \"Address\": \"salem\", \"City\": \"salem\", \"Country\": \"india\", \"County\": \"india\", \"DateOfBirth\": \"sep5\", \"Forename\": \"TEST\", \"Middlename\": \"string\", \"Postcode\": \"636030\", \"Surname\": \"TEST2\", \"YearOfBirth\": \"1992\" }}";
+
+                        streamWriter1.Write(json);
+                        streamWriter1.Flush();
+                    }
+                    var httpResponse1 = (HttpWebResponse)httpWebRequest1.GetResponse();
+                    status1 = httpResponse1.StatusCode.ToString();
+                    value1.API2_Status = status1;
+                }
+                stopWatch.Stop();
+                value1.TaskEnd = DateTime.Now.ToString("HH:mm:ss");
+
+
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe following Exception was raised : {0}", e.Message);
+            }
+            lock (_object)
+            {
+                listvalue.Add(value1);
+            }
+            return Task.FromResult(value1.name);
+        }
+
+    }
+
+
+    public class modelproperty
+        {
+            public string API1_Status = string.Empty;
+            public string API2_Status = string.Empty;
+            public string Totaltime = string.Empty;
+            public string TaskStart = string.Empty;
+            public string TaskEnd = string.Empty;
+            public string name = string.Empty;
+        }
+        public class KYC_VALUE
+        {
+            public string Kycreason = string.Empty;
+            public string Kycresult = string.Empty;
+        }
+    }
+
