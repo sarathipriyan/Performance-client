9131f032abd4b2cf1fbcd46de218fd2f11e31082
sarathipriyan <54711117+sarathipriyan@users.noreply.github.com>
2019-12-03 13:14:42 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,31 @@
+﻿USE [test]  
+GO  
+/****** Object:  Table [dbo].[Plant]    Script Date: 2/2/2019 9:15:36 PM ******/  
+SET ANSI_NULLS ON  
+GO  
+SET QUOTED_IDENTIFIER ON  
+GO  
+CREATE TABLE [dbo].[Plant](  
+    [PlantId] [int] IDENTITY(1,1) NOT NULL,  
+    [Name] [varchar](255) NULL,  
+    [TypeofDistributedEnergy] [varchar](255) NULL,  
+    [Powerdistributor] [int] NULL,  
+    [installedCapacity] [float] NULL,  
+    [Source] [varchar](255) NULL,  
+    [PowerBillExpectedDate] [DATE] NULL,  
+ CONSTRAINT [PK_Plant] PRIMARY KEY CLUSTERED   
+(  
+    [PlantId] ASC  
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]  
+) ON [PRIMARY] 
+GO  
+SET IDENTITY_INSERT [dbo].[Plant] ON   
+GO  
+INSERT [dbo].[Plant] ([PlantId], [Name], [TypeofDistributedEnergy], [Powerdistributor], [installedCapacity], [Source], [PowerBillExpectedDate]) VALUES (111, N'WAY2', N'TypeofDistributedEnergy', 20, 25.4, N'Source', N'2019-11-14')  
+GO  
+INSERT [dbo].[Plant] ([PlantId], [Name], [TypeofDistributedEnergy], [Powerdistributor], [installedCapacity], [Source], [PowerBillExpectedDate]) VALUES (112, N'WAY2', N'TypeofDistributedEnergy', 20, 25.4, N'Source', N'2019-11-15')  
+GO  
+SET IDENTITY_INSERT [dbo].[Plant] OFF  
+GO 
+
+select * From Plant
---
+++ SQLQuery22.sql
@@ -0,0 +1,27 @@
+﻿USE [sarathidemo]
+GO  
+/****** Object:  Table [dbo].[Employee]    Script Date: 2/2/2019 9:15:36 PM ******/  
+SET ANSI_NULLS ON  
+GO  
+SET QUOTED_IDENTIFIER ON  
+GO  
+CREATE TABLE [dbo].[sarathi](  
+    [Id] [bigint] IDENTITY(1,1) NOT NULL,  
+    [Name] [varchar](100) NULL,  
+ CONSTRAINT [PK_sarathi] PRIMARY KEY CLUSTERED   
+(  
+    [Id] ASC  
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]  
+) ON [PRIMARY]   
+GO  
+SET IDENTITY_INSERT [dbo].[sarathi] ON   
+GO  
+INSERT [dbo].[sarathi] ([Id], [Name]) VALUES (1, N'Akshay')
+GO  
+INSERT [dbo].[sarathi] ([Id], [Name]) VALUES (2, N'Panth')
+GO  
+SET IDENTITY_INSERT [dbo].[sarathi] OFF  
+GO 
+
+
+select * from sarathi;
